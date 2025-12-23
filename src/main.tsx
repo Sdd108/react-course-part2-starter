e091fc3f135b4e414fcbd5b6533c4792f68a41c5
“Zhipeng <zhipengmail@qq.com>
2025-12-23 20:33:58 +0800

    Using React Query DevTools

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App";
 import "./index.css";
 
@@ -11,6 +12,8 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      {/* React Query Devtools for debugging */}
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
 );
