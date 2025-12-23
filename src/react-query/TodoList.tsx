dd24f3bf86cd600c79d1bc5f4cb19e0733464b89
“Zhipeng <zhipengmail@qq.com>
2025-12-23 20:12:51 +0800

    Handle errors

--- src/react-query/TodoList.tsx
+++ src/react-query/TodoList.tsx
@@ -14,13 +14,13 @@ const TodoList = () => {
       .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
       .then((res) => res.data);
 
-  const { data: todos } = useQuery({
+  const { data: todos, error } = useQuery<Todo[], Error>({
     // Properties of query: data, error, isLoading, isError, refetch, etc.
     queryKey: ["todos"], // A unique identifier for the query, for caching purposes.  Can be set to an array with multiple values.
     queryFn: fetchTodos, // The function that fetches the data.
   });
 
-  // if (error) return <p>{error}</p>;
+  if (error) return <p>{error.message}</p>;
 
   return (
     <ul className="list-group">
