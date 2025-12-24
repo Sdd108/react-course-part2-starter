83910c549663f740034f6aacff8316d063c7d662
“Zhipeng <zhipengmail@qq.com>
2025-12-24 21:20:04 +0800

    Implement Optimistic Update

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -3,15 +3,36 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { type Todo } from "./hooks/useTodo";
 import axios from "axios";
 
+interface AddTodoContext {
+  previousTodos: Todo[];
+}
+
 const TodoForm = () => {
   const queryClient = useQueryClient();
-  const addTodo = useMutation<Todo, Error, Todo>({
+  const addTodo = useMutation<Todo, Error, Todo, AddTodoContext>({
     mutationFn: (todo: Todo) =>
       axios
         .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
         .then((res) => res.data),
+    // Optimistic Update
+    onMutate: (newTodo: Todo) => {
+      const previousTodos = queryClient.getQueryData<Todo[]>(["todos"]) || [];
+
+      queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
+        newTodo,
+        ...(todos || []),
+      ]);
+
+      if (ref.current) ref.current.value = "";
+
+      return { previousTodos };
+    }, // this function is called before the mutation gets executed
     onSuccess: (savedTodo, newTodo) => {
       console.log(savedTodo);
+      // Optimistic update: replace the newTodo with savedTodo if success because newTodo doesn't have the generated id by the backend
+      queryClient.setQueryData<Todo[]>(["todos"], (todos) =>
+        todos?.map((todo) => (todo === newTodo ? savedTodo : todo))
+      );
       // To update the Todo list:
       // Approach #1: invalidate the cache (doesn't work on fake api backend)
       // queryClient.invalidateQueries({
@@ -19,12 +40,17 @@ const TodoForm = () => {
       // });
 
       // Approach #2: update the data in the cache
-      queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
-        savedTodo,
-        ...(todos || []),
-      ]);
+      // queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
+      //   newTodo,
+      //   ...(todos || []),
+      // ]);
 
-      if (ref.current) ref.current.value = "";
+      // if (ref.current) ref.current.value = "";
+    },
+
+    onError: (error, newTodo, context) => {
+      if (!context) return;
+      queryClient.setQueryData<Todo[]>(["todos"], context.previousTodos);
     },
   });
   const ref = useRef<HTMLInputElement>(null);
