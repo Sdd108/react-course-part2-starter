a20e054438b3732c1b8addb2cffe9c7009d3e8e8
“Zhipeng <zhipengmail@qq.com>
2025-12-24 14:20:08 +0800

    Reformat useInfinitePosts

--- src/react-query/hooks/useInfinitePosts.ts
+++ src/react-query/hooks/useInfinitePosts.ts
@@ -15,15 +15,15 @@ interface PostQuery {
 const useInfinitePosts = (query: PostQuery) => {  
   return useInfiniteQuery<Post[], Error>({
     queryKey: ["users", query, "posts"],  // similar to URL pattern: /users/1/posts
-    queryFn: ({ pageParam = 1 }) =>
-  axios
-    .get<Post[]>("https://jsonplaceholder.typicode.com/posts", {
-      params: {
-        _start: (pageParam - 1) * query.pageSize,
-        _limit: query.pageSize
-      },
-    })
-    .then((res) => res.data),
+    queryFn: ({ pageParam = 1 }) => 
+      axios
+        .get<Post[]>("https://jsonplaceholder.typicode.com/posts", {
+          params: {
+            _start: (pageParam - 1) * query.pageSize,
+            _limit: query.pageSize
+          },
+        })
+        .then((res) => res.data),
     staleTime: 1 * 60 * 1000, // 1 min
     keepPreviousData: true, // 获取下一页时，保持当前页内容直到获取下一页内容，然后瞬间替换
     getNextPageParam: (lastPage, allPages) => {
