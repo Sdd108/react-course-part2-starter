de5283b6190b56c01d6900fa658faff4cd47021c
“Zhipeng <zhipengmail@qq.com>
2025-12-24 21:00:39 +0800

    Mutating data

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import "./App.css";
 import PostFlow from "./react-query/PostFlow";
 import PostList from "./react-query/PostList";
+import TodoForm from "./react-query/TodoForm";
 import TodoList from "./react-query/TodoList";
 
 function App() {
-  return <PostFlow />;
+  return (
+    <>
+      <TodoForm />
+      <TodoList />
+    </>
+  );
 }
 
 export default App;
