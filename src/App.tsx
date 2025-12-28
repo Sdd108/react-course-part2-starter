e5735c4ed05da3e4dbcdb406bc4cd2547a234b39
“Zhipeng <zhipengmail@qq.com>
2025-12-28 17:51:57 +0800

    Implement counterReducer

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import PostFlow from "./react-query/PostFlow";
 import PostList from "./react-query/PostList";
 import TodoForm from "./react-query/TodoForm";
 import TodoList from "./react-query/TodoList";
+import Counter from "./state-management/Counter";
 
 function App() {
   return (
     <>
       {/* <TodoForm />
       <TodoList /> */}
-      <PostList />
+      {/* <PostList /> */}
+      <Counter />
     </>
   );
 }
