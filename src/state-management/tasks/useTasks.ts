f12c1e4be04bdcc2cc59cd38b9381686c8e2a644
“Zhipeng <zhipengmail@qq.com>
2026-01-01 19:11:40 +0800

    Refactor: encapsulate tasks

--- src/state-management/tasks/useTasks.ts
+++ src/state-management/tasks/useTasks.ts
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import TaskContext from "../contexts/tasksContext";
+import TaskContext from "./tasksContext";
 
 const useTasks = () => useContext(TaskContext);
 
