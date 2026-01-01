c3c3cbd5b5361e393a38fdeb0749d4961fd0215d
“Zhipeng <zhipengmail@qq.com>
2026-01-01 22:16:42 +0800

    Prevent unnecessary re-render using selector with Zustand

--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -2,7 +2,13 @@ import { LoginStatus } from "./auth-zustand";
 import useCounterStore from "./counter/store";
 
 const NavBar = () => {
-  const { counter } = useCounterStore();
+  // Changing from this:
+  // const { counter } = useCounterStore();
+  // to this blow: will only re-render if the value of counter changes, not the max
+  // the selector function: (s) => s.counter
+  const counter = useCounterStore((s) => s.counter);
+
+  console.log("Render NavBar");
 
   return (
     <nav className="navbar d-flex justify-content-between">
--- src/state-management/counter/store.ts
+++ src/state-management/counter/store.ts
@@ -2,12 +2,14 @@ import { create } from 'zustand';
 
 interface CounterStore {
   counter: number;
+  max: number;
   increment: () => void;
   reset: () => void;
 }
 
 const useCounterStore = create<CounterStore>(set => ({
   counter: 0,
+  max: 5,
   /** In the set() function, no need to use spread operator to copy the other properties;
    * set() will MERGE the updated value into our next state object.
    * The set() function merges state at only one level.
@@ -15,7 +17,7 @@ const useCounterStore = create<CounterStore>(set => ({
    * You will use the spread operator pattern
    */
   increment: () => set(store => ({counter: store.counter + 1})),
-  reset: () => set(() => ({counter: 0}))
+  reset: () => set(() => ({max: 5}))
 }));
 
 export default useCounterStore;
