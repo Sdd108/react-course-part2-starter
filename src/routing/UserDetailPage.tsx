ad2d5ec90d99e5621d72e10874bd7a31a31e7552
“Zhipeng <zhipengmail@qq.com>
2026-01-03 10:06:14 +0800

    Implement dynamic routing

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,4 +1,18 @@
+import { useLocation, useParams, useSearchParams } from "react-router-dom";
+
 const UserDetailPage = () => {
+  // Approach #1
+  const param = useParams();
+  console.log(param);
+
+  // Approach #2
+  const [searchParams, setSearchParams] = useSearchParams();
+  console.log(searchParams.get("name"));
+
+  // Approach #3
+  const location = useLocation();
+  console.log(location);
+
   return <p>User</p>;
 };
 
